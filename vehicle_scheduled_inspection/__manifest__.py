6c7330f148f6ca76bd2b1b15252f3eb6a6735c81
RPSJR <raimundopsjr@gmail.com>
2026-05-01 16:27:17 -0300

    [NEW] add vehicle_scheduled_inspection

---
+++ vehicle_scheduled_inspection/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Vehicle Scheduled Inspection",
+    "version": "13.0.1.0.0",
+    "category": "Fleet",
+    "summary": "Schedule vehicle inspections based on maintenance plan.",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "fleet",
+        "fleet_vehicle_inspection",
+        "fleet_vehicle_model_inspection_plan",
+        "vehicle_parts_catalogue",
+        "meta_whatsapp",
+    ],
+    "data": [
+        "data/cron_data.xml",
+        "data/mail_template_data.xml",
+    ],
+    "installable": True,
+}
